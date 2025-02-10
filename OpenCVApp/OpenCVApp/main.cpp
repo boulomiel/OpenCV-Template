2c5426351754bcbfa3a702948ffbb3fa041bb7ca
boulomiel <55102143+boulomiel@users.noreply.github.com>
2025-02-10 07:36:51 +0200

    First commit
    Added base files to start a project

---
+++ OpenCVApp/OpenCVApp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  OpenCVApp
+//
+//  Created by Ruben Mimoun on 10/02/2025.
+//
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cv::Mat hello;
+    return 0;
+}
